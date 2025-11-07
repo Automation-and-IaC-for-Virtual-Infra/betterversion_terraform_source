626d3f4ffb46c31e8f794d67e4bf4de55c5eb46a
KhuongDuy-1110 <khuongc3@gmail.com>
2025-11-08 00:09:33 +0700

    fix: sg

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,7 +1,6 @@
 
 # Data source for security group by name (when not in spec.security_groups)
-data "aws_security_group" "__fn__getatt_____1000000000000011____groupid___" {
-  name = "{'Fn::GetAtt': ['1000000000000011', 'GroupId']}"
+data "aws_security_group" "app" {
   vpc_id = module.vpc.vpc_id
 }
 
@@ -10,7 +9,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = var.ec2_ec2_instance_instance_type
   subnet_id              = var.ec2_ec2_instance_subnet_id
   # When subnet_id is present, must use vpc_security_group_ids (IDs), not security_groups (names)
-  vpc_security_group_ids = [data.aws_security_group.__fn__getatt_____1000000000000011____groupid___.id,]
+  vpc_security_group_ids = [data.aws_security_group.app.id,]
   tags                   = var.ec2_ec2_instance_tags
 }
 
