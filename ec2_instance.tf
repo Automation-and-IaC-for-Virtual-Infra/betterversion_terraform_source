cde2a9f23db382c0dea05cd55a79a9a371e489ea
tuannguyenzz <136990882+tuannguyenzz@users.noreply.github.com>
2025-11-08 00:31:57 +0700

    Add ec2_instance.tf - 2025-11-07-17-31-55

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,16 +1,17 @@
 
 # Data source for security group by name (when not in spec.security_groups)
-data "aws_security_group" "app" {
+data "aws_security_group" "__fn__getatt_____9805172638405172____groupid___" {
+  name = "{'Fn::GetAtt': ['9805172638405172', 'GroupId']}"
   vpc_id = module.vpc.vpc_id
 }
 
-resource "aws_instance" "ec2_instance" {
-  ami                    = var.ec2_ec2_instance_ami
-  instance_type          = var.ec2_ec2_instance_instance_type
-  subnet_id              = var.ec2_ec2_instance_subnet_id
+resource "aws_instance" "ubuntuwebserverinstance" {
+  ami                    = var.ec2_ubuntuwebserverinstance_ami
+  instance_type          = var.ec2_ubuntuwebserverinstance_instance_type
+  subnet_id              = var.ec2_ubuntuwebserverinstance_subnet_id
   # When subnet_id is present, must use vpc_security_group_ids (IDs), not security_groups (names)
-  vpc_security_group_ids = [data.aws_security_group.app.id,]
-  tags                   = var.ec2_ec2_instance_tags
+  vpc_security_group_ids = [data.aws_security_group.__fn__getatt_____9805172638405172____groupid___.id,]
+  tags                   = var.ec2_ubuntuwebserverinstance_tags
 }
 
 
