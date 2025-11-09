eaa6d465ad1883e1d2e59a1f0c9719c060f2748b
tuannguyenzz <136990882+tuannguyenzz@users.noreply.github.com>
2025-11-09 23:27:31 +0700

    Add variables.tf - 2025-11-09-16-27-21

--- variables.tf
+++ variables.tf
@@ -1,9 +1,55 @@
 variable "region" {
   description = "aws region"
-  default = "ap-southeast-1"
+  type        = string
+  default     = "ap-southeast-1"
 }
 
-variable "public_key" {
+variable "asg_ami_id" {
+  description = "AMI used for ASG"
   type        = string
-  description = "SSH public key content"
+  default     = "ami-0abcde1234567890f"
+}
+
+variable "ec2_ec2_instance_ami" {
+  description = "EC2 EC2 Instance AMI"
+  type        = string
+  default     = "ami-0a0e1042571252119"
+}
+
+variable "ec2_ec2_instance_instance_type" {
+  description = "EC2 EC2 Instance instance type"
+  type        = string
+  default     = "t3.small"
+}
+
+variable "ec2_ec2_instance_subnet_id" {
+  description = "EC2 EC2 Instance subnet id"
+  type        = string
+  default     = "webserver_public_subnet_a"
+}
+
+variable "ec2_ec2_instance_tags" {
+  description = "EC2 EC2 Instance tags"
+  type        = map(string)
+  default     = {
+  Name = "NginxInstance"
+}
+}
+
+variable "sg_security_group_name" {
+  description = "Security group name"
+  type        = string
+  default     = "Security Group"
+}
+
+variable "sg_security_group_vpc_id" {
+  description = "VPC id for SG Security Group"
+  type        = string
+  default     = "webserver_vpc"
+}
+
+variable "sg_security_group_description" {
+  description = "Description for SG Security Group"
+  type        = string
+  default     = "Allow HTTP and SSH access to webserver"
 }
