0114927de84df9dccf2b7c686a4c2ee6461d1ac8
tuannguyenzz <136990882+tuannguyenzz@users.noreply.github.com>
2025-11-09 23:27:23 +0700

    Add ec2_instance.tf - 2025-11-09-16-27-21

---
+++ ec2_instance.tf
@@ -0,0 +1,17 @@
+
+# Data source for security group by name (when not in spec.security_groups)
+data "aws_security_group" "webserver_sg" {
+  name = "webserver_sg"
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami                    = var.ec2_ec2_instance_ami
+  instance_type          = var.ec2_ec2_instance_instance_type
+  subnet_id              = var.ec2_ec2_instance_subnet_id
+  # When subnet_id is present, must use vpc_security_group_ids (IDs), not security_groups (names)
+  vpc_security_group_ids = [data.aws_security_group.webserver_sg.id,]
+  tags                   = var.ec2_ec2_instance_tags
+}
+
+
