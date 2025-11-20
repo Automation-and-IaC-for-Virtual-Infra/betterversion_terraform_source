2504a7e181b8c873c14b3c0e031fc4392fd1acac
tuannguyenzz <136990882+tuannguyenzz@users.noreply.github.com>
2025-11-20 21:51:51 +0700

    Add variables.tf - 2025-11-20-14-51-41

--- variables.tf
+++ variables.tf
@@ -1,9 +1,43 @@
 variable "region" {
   description = "aws region"
-  default = "ap-southeast-1"
+  type        = string
+  default     = "ap-southeast-1"
 }
 
-variable "public_key" {
+variable "ec2_ec2_instance_ami" {
+  description = "EC2 EC2 Instance AMI"
   type        = string
-  description = "SSH public key content"
+  default     = "ami-0abcdef1234567890"
+}
+
+variable "ec2_ec2_instance_instance_type" {
+  description = "EC2 EC2 Instance instance type"
+  type        = string
+  default     = "t3.small"
+}
+
+variable "ec2_ec2_instance_subnet_id" {
+  description = "EC2 EC2 Instance subnet id"
+  type        = string
+  default     = "ubuntu_public_subnet_a"
+}
+
+variable "ec2_ec2_instance_tags" {
+  description = "EC2 EC2 Instance tags"
+  type        = map(string)
+  default     = {
+  Name = "UbuntuInstance"
+}
+}
+
+variable "sg_security_group_name" {
+  description = "Security group name"
+  type        = string
+  default     = "Security Group"
+}
+
+variable "sg_security_group_description" {
+  description = "Description for SG Security Group"
+  type        = string
+  default     = "Allow HTTP and SSH access"
 }
